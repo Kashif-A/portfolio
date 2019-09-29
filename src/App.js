2280d4c23570f1668795cca912fbd9517bf5513c
Kashif Ahmed <kashif.ahmed@macbook-pro-20.local>
2019-09-29 12:12:32 +0100

    Styling refactor

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div styles={styles.container}>
+      <header style={styles.header}>
+       
       </header>
     </div>
   );
 }
 
 export default App;
+
+const styles = {
+  container: {
+    'text-align': 'center'
+  },
+  header: {
+    'background-color': '#282c34',
+    'min-height': '100vh',
+    'display': 'flex',
+    'flex-direction': 'column',
+    'align-items': 'center',
+    'justify-content': 'center',
+    'font-size': 'calc(10px + 2vmin)',
+    'color': 'white'
+    }
+}
+
